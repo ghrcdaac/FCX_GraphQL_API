286cfe127d2c5b4398fabe5aecf098e97c39d579
Sanjog <st0128@uah.edu>
2023-10-26 11:44:06 -0500

    campaign models registered to admin panel

--- fcxbackendapi/campaign_layers/admin.py
+++ fcxbackendapi/campaign_layers/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Link, DOI, Legend, InstrumentLayer, CampaignLayer
+
+admin.site.register(Link)
+admin.site.register(DOI)
+admin.site.register(Legend)
+admin.site.register(InstrumentLayer)
+admin.site.register(CampaignLayer)
