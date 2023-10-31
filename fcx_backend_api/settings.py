1c94f3db49e70fa2c053645e09bd805a914732a2
Sanjog <st0128@uah.edu>
2023-10-31 13:10:13 -0500

    dockerize and add pgsql wip

--- fcx_backend_api/settings.py
+++ fcx_backend_api/settings.py
@@ -74,11 +74,15 @@ WSGI_APPLICATION = 'fcx_backend_api.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+import os
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'pgsqldb',
+        'PORT': 5432
     }
 }
 
