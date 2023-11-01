efed375bddc2aee7fc301a91e63cf152900f30af
Sanjog <st0128@uah.edu>
2023-11-01 12:15:14 -0500

    1. added needed modules for postgres in django. 2. removed unnecessary fixtures dump and updated seed file. 3. changed docker compose for api to correctly map ip:ports 4. updated names for services in docker compose.

--- fcx_backend_api/settings.py
+++ fcx_backend_api/settings.py
@@ -81,11 +81,10 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_NAME'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'pgsqldb',
+        'HOST': 'db',
         'PORT': 5432
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
